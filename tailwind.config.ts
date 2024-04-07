f96c74ca2a735cfac91f6e1f9eadc9a8bd0490f1
Collection50 <fkaus65@naver.com>
2024-04-07 22:25:17 +0900

    initial setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+const px0To10 = {
+  ...Array.from(Array(11)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+};
+const px0To100 = {
+  ...Array.from(Array(101)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+};
+const px0To500 = {
+  ...Array.from(Array(501)).reduce(
+    (acc, _, i) => ({ ...acc, [i]: `${i}px` }),
+    {},
+  ),
+};
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      width: px0To500,
+      height: px0To500,
+      borderWidth: px0To10,
+      fontSize: px0To100,
+      lineHeight: px0To100,
+      minWidth: px0To500,
+      minHeight: px0To500,
+      spacing: px0To500,
+      borderRadius: { ...px0To100, button: 6 },
     },
   },
   plugins: [],
