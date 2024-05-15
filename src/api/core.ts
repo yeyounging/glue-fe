a254064077c0987232bac20e239cd12ade5c1513
Collection50 <fkaus65@naver.com>
2024-05-15 18:47:39 +0900

    chore: env 파일에 작성된 서버 url 변수명 변경

--- src/api/core.ts
+++ src/api/core.ts
@@ -11,7 +11,7 @@ import { ACCESS_TOKEN, HTTP_METHODS } from '@/constants';
 import { BaseResponse } from './types';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
