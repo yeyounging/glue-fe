70483a5f043ca9a5e31c4cdd15f8d02a19f0d621
Collection50 <fkaus65@naver.com>
2024-05-19 22:16:52 +0900

    refactor: fontStyle 객체에 as const 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import { KakaoLoginIcon } from '@/components/Common';
 
 export default function Page() {
-  const styles = {
+  const fontStyle = {
     fontFamily:
       'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
-  };
+  } as const;
 
   return (
     <section className="w-full h-full bg-primary flex justify-center items-center">
@@ -13,7 +13,7 @@ export default function Page() {
         <h1 className="font-luckiest text-30">LOGIN</h1>
 
         <Link
-          style={styles}
+          style={fontStyle}
           className="relative w-full h-45 text-15 font-semibold px-14 flex justify-center items-center rounded-button bg-[#FEE500]"
           href={process.env.NEXT_PUBLIC_LOGIN_URL!}
         >
