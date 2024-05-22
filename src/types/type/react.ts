7105d8a2b5c07f92578cc1e58acab126afc2cfca
Collection50 <fkaus65@naver.com>
2024-05-22 23:13:27 +0900

    feat: 리액트에 사용되는 type 추가

--- src/types/type/react.ts
+++ src/types/type/react.ts
@@ -1,3 +1,13 @@
-import { ReactNode } from 'react';
+import {
+  ReactNode,
+  ElementType,
+  RefObject,
+  MutableRefObject,
+  Ref,
+} from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type As<Props = any> = ElementType<Props>;
+export type ReactRef<T> = RefObject<T> | MutableRefObject<T> | Ref<T>;
 
 export type StrictPropsWithChildren<P = unknown> = P & { children: ReactNode };
