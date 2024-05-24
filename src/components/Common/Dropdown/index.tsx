73e7a6d6aef5e95600f59499c821e83e906c0ae6
Collection50 <fkaus65@naver.com>
2024-05-24 14:56:37 +0900

    feat: 드롭다운 컴포넌트 구현

---
+++ src/components/Common/Dropdown/index.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { ButtonHTMLAttributes, useMemo } from 'react';
+import { mergeProps } from '@react-aria/utils';
+import { AnimationProps, motion } from 'framer-motion';
+import { StrictPropsWithChildren } from '@/types';
+import { cn } from '@/utils';
+import Button from '../Button';
+import { useDropdown, UseDropdownProps } from './useDropdown';
+import { DropdownProvider, useDropdownContext } from './DropdownContext';
+
+interface DropdownProps extends UseDropdownProps, StrictPropsWithChildren {
+  value?: string;
+}
+
+function Dropdown({ children, ...props }: DropdownProps) {
+  const { Component, getBaseProps, ...rest } = useDropdown(props);
+
+  return (
+    <Component {...getBaseProps()}>
+      <DropdownProvider {...rest}>{children}</DropdownProvider>
+    </Component>
+  );
+}
+
+function Trigger({
+  children,
+  ...props
+}: StrictPropsWithChildren &
+  Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>) {
+  const {
+    context: { onOpenChange },
+  } = useDropdownContext()!;
+
+  return (
+    <Button {...props} onClick={() => onOpenChange()}>
+      {children}
+    </Button>
+  );
+}
+
+function Menu({ children }: StrictPropsWithChildren) {
+  const { context, getMenuProps } = useDropdownContext()!;
+  const { isOpen } = context;
+  const dropdownAnimations: AnimationProps = useMemo(
+    () => ({
+      initial: { opacity: 0, y: -10 },
+      animate: { opacity: 1, y: 0 },
+      transition: { duration: 0.3 },
+      exit: { opacity: 0, y: -10 },
+    }),
+    [],
+  );
+
+  return (
+    <div {...getMenuProps()}>
+      {isOpen && <motion.div {...dropdownAnimations}>{children}</motion.div>}
+    </div>
+  );
+}
+
+function Item({
+  children,
+  className,
+  ...props
+}: StrictPropsWithChildren &
+  Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onChange'>) {
+  const { getItemButtonProps } = useDropdownContext()!;
+
+  return (
+    <Button
+      {...mergeProps(getItemButtonProps(), props)}
+      className={cn(
+        'p-0 w-full h-28 !rounded-0 bg-[white/0.85] text-[black] text-[16px]',
+        className,
+      )}
+    >
+      {children}
+    </Button>
+  );
+}
+
+Dropdown.Trigger = Trigger;
+Dropdown.Menu = Menu;
+Dropdown.Item = Item;
+
+export default Dropdown;
---
+++ src/components/Common/Dropdown/useDropdown.ts
@@ -0,0 +1,83 @@
+'use client';
+
+import { ChangeEvent, useCallback, useMemo, useRef } from 'react';
+import { chain } from '@react-aria/utils';
+import { As, ClassNamesType } from '@/types';
+import { cn } from '@/utils';
+import useDOMRef from '@/hooks/useDOMRef';
+import useDisclosure from '@/hooks/useDisclosure';
+import useOutsideClick from '@/hooks/useOutsideClick';
+
+export interface UseDropdownProps {
+  as?: As;
+  onChange?: (value: string) => void;
+  classNames?: ClassNamesType<'base' | 'item' | 'menu'>;
+}
+
+export function useDropdown({ ...props }: UseDropdownProps) {
+  const { as, classNames, onChange } = props;
+  const { isOpen, onOpenChange, onOpen, onClose } = useDisclosure();
+
+  const Component = as || 'div';
+
+  const ref = useRef<HTMLDivElement>(null);
+  const domRef = useDOMRef(ref);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useOutsideClick(domRef, onClose);
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLButtonElement>) => {
+      onChange?.(e.target.value);
+    },
+    [onChange],
+  );
+
+  const context = useMemo(
+    () => ({
+      isOpen,
+      onOpenChange,
+      onClose,
+      onOpen,
+    }),
+    [isOpen, onOpenChange, onOpen, onClose],
+  );
+
+  const getBaseProps = useCallback(
+    () => ({
+      ref: domRef,
+      className: cn('relative', classNames?.base),
+    }),
+    [domRef, classNames?.base],
+  );
+
+  const getMenuProps = useCallback(
+    () => ({
+      className: cn(
+        'absolute top-27 w-full z-[10] rounded-3 dropdown-blur bg-[white/0.85]',
+        isOpen && 'border-1 border-[white] shadow-drodpown',
+        classNames?.menu,
+      ),
+    }),
+    [classNames?.menu, isOpen],
+  );
+
+  const getItemButtonProps = useCallback(
+    () => ({
+      ref: buttonRef,
+      onClick: chain(handleChange, onClose),
+      className: cn(classNames?.item),
+    }),
+    [buttonRef, classNames?.item, onClose, handleChange],
+  );
+
+  return {
+    Component,
+    getBaseProps,
+    getItemButtonProps,
+    getMenuProps,
+    context,
+  };
+}
+
+export type UseDropdownReturn = ReturnType<typeof useDropdown>;
