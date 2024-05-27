5df4888935305d770a5651d00c26c5176d0e3347
Collection50 <fkaus65@naver.com>
2024-05-27 10:36:49 +0900

    feat: 해시태그 핸들링을 관리하는 useHashtags hook 구현

---
+++ src/app/write/hooks/useHashtags.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { KeyboardEvent, useState, CompositionEvent, useCallback } from 'react';
+
+export default function useHashtags() {
+  const [value, setValue] = useState<string>('');
+  const [hashtags, setHashtags] = useState<string[]>([]);
+  const [isComposing, setIsComposing] = useState<boolean>(false);
+
+  const handleComposition = (e: CompositionEvent<HTMLInputElement>) => {
+    setIsComposing(e.type !== 'compositionend');
+  };
+
+  const addHashtag = useCallback(() => {
+    setHashtags((prevHashtags) => [...prevHashtags, value.trim()]);
+    setValue('');
+  }, [value]);
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter' && !isComposing && value.trim() !== '') {
+        e.preventDefault();
+        addHashtag();
+      }
+    },
+    [addHashtag, isComposing, value],
+  );
+
+  const handleDelete = useCallback((index: number) => {
+    setHashtags((prevHashtags) => prevHashtags.filter((_, i) => i !== index));
+  }, []);
+
+  const getInputProps = useCallback(
+    () => ({
+      value,
+      onValueChange: setValue,
+      onKeyDown: handleKeyDown,
+      onCompositionStart: handleComposition,
+      onCompositionEnd: handleComposition,
+      placeholder: '해시태그를 입력하세요.',
+    }),
+    [handleKeyDown, value],
+  );
+
+  return {
+    getInputProps,
+    handleDelete,
+    hashtags,
+  };
+}
