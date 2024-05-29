616b5b3baf7320b9bf34687e0b6fec380fb52812
Collection50 <fkaus65@naver.com>
2024-05-29 14:45:13 +0900

    feat: 글 삭제 api, query 구현

--- src/app/blog/[blogId]/post/[postId]/components/PostDetailHeader/api/index.tsx
+++ src/app/blog/[blogId]/post/[postId]/components/PostDetailHeader/api/index.tsx
@@ -2,3 +2,6 @@ import { http } from '@/api';
 
 export const postFollow = (postId: number) =>
   http.post({ url: `/blogs/subscriptions/${postId}` });
+
+export const deletePosting = (postId: number) =>
+  http.delete({ url: `/posts/${postId}` });
