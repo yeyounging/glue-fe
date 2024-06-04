3edf7486d9c4feb7ddca262e36bb9b02d1622e83
Collection50 <fkaus65@naver.com>
2024-06-04 10:04:10 +0900

    feat: 스티커를 렌더링하기 위한 response 타입 추가

--- src/app/blog/[blogId]/post/[postId]/components/PostDetailFetcher/types.ts
+++ src/app/blog/[blogId]/post/[postId]/components/PostDetailFetcher/types.ts
@@ -1,5 +1,21 @@
 import { PartialBlock } from '@blocknote/core';
 
+interface Sticker {
+  postStickerItem: {
+    xlocation: number;
+    ylocation: number;
+    postStickerId: number;
+    postId: number;
+    stickerId: number;
+    xLocation: number;
+    yLocation: number;
+    width: number;
+    height: number;
+    angle: number;
+  };
+  url: string;
+}
+
 export interface PostDetailResponse {
   postDetail: {
     postId: number;
@@ -17,6 +33,12 @@ export interface PostDetailResponse {
     isSubscribe: boolean;
     categoryName: string;
     hashtags: string[];
+
+    photoUrls: string[];
+
+    postStickerUrlItems: {
+      postStickerId: Sticker[];
+    };
   };
 
   loginMemberId: number;
