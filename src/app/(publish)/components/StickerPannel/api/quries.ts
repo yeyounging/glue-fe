aa469aa03a201be1a8a3e573d15c2009d315274e
Collection50 <fkaus65@naver.com>
2024-06-07 01:59:43 +0900

    fix: 블로그 글 상세 페이지의 최소 높이 설정

--- src/app/(publish)/components/StickerPannel/api/quries.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { postGenerateSticker } from '.';
-
-export const useGenerateSticker = () =>
-  useMutation({
-    mutationKey: ['generate-sticker'],
-    mutationFn: (query: string) => postGenerateSticker(query),
-  });
