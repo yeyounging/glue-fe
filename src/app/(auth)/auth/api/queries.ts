a9e3c929c24f544d4521a8d6c940007a62f1ca92
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-06-12 12:33:24 +0900

    fix: loginId전역관리 오류 수정 (#89)

--- src/app/(auth)/auth/api/queries.ts
+++ src/app/(auth)/auth/api/queries.ts
@@ -1,4 +1,3 @@
-import { getQueryClient } from '@/app/lib';
 import { useMutation } from '@tanstack/react-query';
 import { ACCESS_TOKEN } from '@/constants';
 import Cookies from 'js-cookie';
@@ -15,7 +14,6 @@ export const usePostLogin = (code: string) => {
     onSuccess: ({ result: { accessToken, blogId } }) => {
       Cookies.set(ACCESS_TOKEN, accessToken);
       setLoginId(blogId);
-      getQueryClient().invalidateQueries({ queryKey: ['login'] });
     },
   });
 };
